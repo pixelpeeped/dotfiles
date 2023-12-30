826d01a9f70e01d94c094abb95939d38f718df8e
Saksham Kumar <sakshamkmr21@gmail.com>
2023-12-30 12:47:13 +0530

    Add keymaps to enter empty line nvim

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Go to left 
 vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Go to lower window", remap = true })
 vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Go to upper window", remap = true })
 vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Go to right window", remap = true })
+
+vim.keymap.set("n", "<Enter>", "o<ESC>", { desc = "Add empty line below" })
+vim.keymap.set("n", "<S-Enter>", "O<ESC>", { desc = "Add empty line above" })
