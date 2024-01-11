4fd13fb1e334e2c30c1c58276eb854fca89e0677
Saksham Kumar <sakshamkmr21@gmail.com>
2024-01-11 23:36:51 +0530

    Add border styles

---
+++ neovim/.config/nvim/lua/utils.lua
@@ -0,0 +1,15 @@
+local U = {}
+U.border_chars_round = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+U.border_chars_none = { "", "", "", "", "", "", "", "" }
+U.border_chars_empty = { " ", " ", " ", " ", " ", " ", " ", " " }
+U.border_chars_inner_thick = { " ", "▄", " ", "▌", " ", "▀", " ", "▐" }
+U.border_chars_outer_thick = { "▛", "▀", "▜", "▐", "▟", "▄", "▙", "▌" }
+U.border_chars_cmp_items = { "▛", "▀", "▀", " ", "▄", "▄", "▙", "▌" }
+U.border_chars_cmp_doc = { "▀", "▀", "▀", " ", "▄", "▄", "▄", "▏" }
+U.border_chars_outer_thin = { "🭽", "▔", "🭾", "▕", "🭿", "▁", "🭼", "▏" }
+U.border_chars_inner_thin = { " ", "▁", " ", "▏", " ", "▔", " ", "▕" }
+U.border_chars_outer_thin_telescope = { "▔", "▕", "▁", "▏", "🭽", "🭾", "🭿", "🭼" }
+U.border_chars_outer_thick_telescope = { "▀", "▐", "▄", "▌", "▛", "▜", "▟", "▙" }
+U.border_chars_single_thick_telescope = { "━", "┃", "━", "┃", "┏", "┓", "┛", "┗" }
+U.border_chars_single_thick = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" }
+return U
