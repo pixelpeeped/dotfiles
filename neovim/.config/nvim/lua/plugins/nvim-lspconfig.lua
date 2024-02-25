4b2b5286def578464ff9bbd686675c8e1c0276bf
Saksham Kumar <sakshamkmr21@gmail.com>
2024-02-25 13:35:45 +0530

    Set diagnostics virtual_text and underline to false by default

--- neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,13 @@
 return {
     "neovim/nvim-lspconfig",
+    opts = {
+        diagnostics = {
+            underline = false,
+            virtual_text = false,
+        },
+    },
     init = function()
+        vim.diagnostic.config({ virtual_text = false, underline = false })
         require("lspconfig.ui.windows").default_options.border = "single"
     end,
 }
